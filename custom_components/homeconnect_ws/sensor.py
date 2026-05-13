12a2c04893dce851d3eb5078952799017fa73db0
Michailjovic <55282780+Michailjovic@users.noreply.github.com>
2026-05-13 17:08:33 +0200

    Refactor HC sensor to use HomeAppliance and DeviceInfo

--- custom_components/homeconnect_ws/sensor.py
+++ custom_components/homeconnect_ws/sensor.py
@@ -7,7 +7,8 @@ from typing import TYPE_CHECKING
 
 from aiohttp.client_exceptions import ClientConnectionResetError
 from homeassistant.components.sensor import SensorEntity
-from homeconnect_websocket import NotConnectedError
+from homeassistant.helpers.device_registry import DeviceInfo
+from homeconnect_websocket import HomeAppliance, NotConnectedError
 
 from .entity import HCEntity
 from .helpers import create_entities
@@ -16,9 +17,11 @@ _LOGGER = logging.getLogger(__name__)
 
 if TYPE_CHECKING:
     from homeassistant.core import HomeAssistant
+    from homeassistant.helpers.entity import DeviceInfo
     from homeassistant.helpers.entity_platform import AddEntitiesCallback
+    from homeconnect_websocket import HomeAppliance
 
-    from . import HCConfigEntry, HCData
+    from . import HCConfigEntry
     from .entity_descriptions.descriptions_definitions import HCSensorEntityDescription
 
 PARALLEL_UPDATES = 0
@@ -50,9 +53,10 @@ class HCSensor(HCEntity, SensorEntity):
     def __init__(
         self,
         entity_description: HCSensorEntityDescription,
-        runtime_data: HCData,
+        appliance: HomeAppliance,
+        device_info: DeviceInfo,
     ) -> None:
-        super().__init__(entity_description, runtime_data)
+        super().__init__(entity_description, appliance, device_info)
 
         if self._entity.enum:
             if self.entity_description.has_state_translation:
@@ -86,7 +90,7 @@ class HCEventSensor(HCEntity, SensorEntity):
 
     @property
     def available(self) -> bool:
-        return self._runtime_data.appliance.session.connected
+        return self._appliance.session.connected
 
 
 class HCActiveProgram(HCSensor):
@@ -97,19 +101,18 @@ class HCActiveProgram(HCSensor):
     def __init__(
         self,
         entity_description: HCSensorEntityDescription,
-        runtime_data: HCData,
+        appliance: HomeAppliance,
+        device_info: DeviceInfo,
     ) -> None:
-        super().__init__(entity_description, runtime_data)
+        super().__init__(entity_description, appliance, device_info)
         self._attr_options = list(entity_description.mapping.values())
 
     @property
     def native_value(self) -> str | None:
-        if self._runtime_data.appliance.active_program:
-            if self._runtime_data.appliance.active_program.name in self.entity_description.mapping:
-                return self.entity_description.mapping[
-                    self._runtime_data.appliance.active_program.name
-                ]
-            return self._runtime_data.appliance.active_program.name
+        if self._appliance.active_program:
+            if self._appliance.active_program.name in self.entity_description.mapping:
+                return self.entity_description.mapping[self._appliance.active_program.name]
+            return self._appliance.active_program.name
         return None
 
 
@@ -121,13 +124,14 @@ class HCWiFI(HCEntity, SensorEntity):
     def __init__(
         self,
         entity_description: HCSensorEntityDescription,
-        runtime_data: HCData,
+        appliance: HomeAppliance,
+        device_info: DeviceInfo,
     ) -> None:
-        super().__init__(entity_description, runtime_data)
+        super().__init__(entity_description, appliance, device_info)
 
     async def async_update(self) -> None:
         try:
-            network_info = await self._runtime_data.appliance.get_network_config()
+            network_info = await self._appliance.get_network_config()
             if network_info and isinstance(network_info, list) and "rssi" in network_info[0]:
                 self._attr_native_value = network_info[0]["rssi"]
             else:
